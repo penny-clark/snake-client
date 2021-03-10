95bc4630b68cc9f3547ad9e7f2be0f5ba58d372a
Penny Clark <respect.cactus@gmail.com>
2021-03-10 19:50:40 +0000

    playing with movement options

--- client.js
+++ client.js
@@ -11,16 +11,25 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  //lets client know they have connected
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
   });
 
+  //received messages from server (eg. when booted because of idling)
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
+  //set username 
   conn.on('connect', () => {
     conn.write("Name: PAC");
+  });
+
+  //requesting snake movement from server
+  conn.on('connect', () => {
+    //hardcoded Move:up - leaving here for reference but it shouldn't be used
+    //setInterval(() => {conn.write("Move: up")}, 50);
   });
 
   return conn;
