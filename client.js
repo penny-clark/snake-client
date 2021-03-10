f66b8e8fabd44ab21399230c8c4e5fa584bea0dc
Penny Clark <respect.cactus@gmail.com>
2021-03-10 19:40:29 +0000

    client snake has name identifier

--- client.js
+++ client.js
@@ -11,8 +11,16 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: PAC");
   });
 
   return conn;
